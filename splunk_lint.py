29455898bfff78236d520e0adef42796426c4ce8
Macias, Bernie <Bernie.Macias@exchange.nordstrom.com>
2014-08-11 13:51:43 -0700

    added license header

--- splunk_lint.py
+++ splunk_lint.py
@@ -1,5 +1,19 @@
 __author__ = 'x243'
 
+#!/usr/bin/env python
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the (LGPL) GNU Lesser General Public License as
+# published by the Free Software Foundation; either version 3 of the 
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Library Lesser General Public License for more details at
+# ( http://www.gnu.org/licenses/lgpl.html ).
+# written by: Bernardo Macias ( httpstergeek@httpstergeek.com )
+
 # Currently require a splunk_lint.cfg file to run
 #
 #
